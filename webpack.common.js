1e9a14c2ce2ecaca39bbea7bb893bacb317b1f19
Felix Wieland <mail@felixwie.com>
2021-02-17 10:44:34 +0100

    feat: webpack build
    Adds webpack bundeling and hot reload.
    New entrypoint for developement is app/src

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: './app/src/index.js',
+	// plugins: [
+	// 	new HtmlWebpackPlugin({
+	// 		title: 'Brainstorming',
+	// 	}),
+	// ],
+	output: {
+		filename: 'build.js',
+		path: path.resolve(__dirname, 'app', 'resources', 'js'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+}

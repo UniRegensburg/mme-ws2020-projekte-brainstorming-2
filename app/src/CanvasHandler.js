8f58d3c11b260184bcf6599faae073e7fd82c2ba
valentinWild <74301124+valentinWild@users.noreply.github.com>
2021-03-02 18:01:48 +0100

    feat: added Function to add Pictures,Squares and Textboxes to canvas and Function to delete them

---
+++ app/src/CanvasHandler.js
@@ -0,0 +1,116 @@
+import { fabric } from "fabric";
+import { isObjectBindingPattern } from "typescript";
+import { Observable, Event } from "./Observable.js";
+import uiElements from "./uiElements.js";
+
+class CanvasHandler extends Observable{
+
+    constructor (canvas){
+        super();
+        this.canvas = canvas;
+        this.contextMenuActive = false;
+        this.selectedObject = null;
+        this.colors = getComputedStyle(document.documentElement);
+        this.activeColor = this.colors.getPropertyValue("--red");
+        this.addContextButtonListeners();
+        this.addListener();
+    }
+
+    addListener(){
+        this.addFileUploadListener();
+        uiElements.BTN_RECT.addEventListener("click", this.createRect.bind(this));
+        uiElements.BTN_TEXT.addEventListener("click", this.createText.bind(this));
+        let colorInputList = uiElements.INPUT_COLORS;
+        for (let i = 0; i < colorInputList.length ; i++) {
+            colorInputList[i].addEventListener("click", (event) => {
+                let color = event.target.value;
+                this.activeColor = this.colors.getPropertyValue(`--${color}`);
+            });
+        }
+    }
+
+    createRect(){
+        let rect = new fabric.Rect({
+            left: 100,
+            top: 100,
+            fill: this.activeColor,
+            width: 100,
+            height: 100,
+            });
+        this.addContextmenuListener(rect);
+        this.canvas.add(rect);
+    }
+
+    createText(){
+        let text = new fabric.IText("Text",{ 
+            left: 100, 
+            top: 100,
+            fill: this.activeColor, 
+            });
+        this.addContextmenuListener(text);
+        this.canvas.add(text);
+    }
+
+    /* Create Contextmenu, when right-clicking on a Canvas-Object */
+
+    addContextmenuListener(object){
+        object.on('mousedown', (event) =>{
+            if(event.button === 3){
+                let menu = uiElements.CONTEXTMENU;
+                    //pointer = this.canvas.getPointer(event.e, true);
+                console.log(this.canvas.findTarget(event.e));
+                this.selectedObject = this.canvas.findTarget(event.e);
+                menu.style.left = `${event.e.clientX}px`;
+                menu.style.top = `${event.e.clientY}px`;
+                menu.style.display = "block";
+                this.contextMenuActive = true;
+            }
+        });
+        window.addEventListener("click", () => {
+            if(this.contextMenuActive){
+                let menu = uiElements.CONTEXTMENU;
+                menu.style.display = "none";
+            }
+        });
+    }
+
+    addContextButtonListeners(){
+        let contextDelete = uiElements.CONTEXTMENU.querySelector("#context-delete"),
+            contextToTop = uiElements.CONTEXTMENU.querySelector("#context-to-top"),
+            contextToBottom = uiElements.CONTEXTMENU.querySelector("#context-to-bottom");
+        let thisinstance = this;
+        contextDelete.addEventListener("click", this.deleteObject.bind(thisinstance));
+        contextToTop.addEventListener("click", () => {
+            this.canvas.bringToFront(this.selectedObject);
+        });
+        contextToBottom.addEventListener("click", () => {
+            this.canvas.sendToBack(this.selectedObject);
+        });
+    }
+
+    addFileUploadListener(){
+        uiElements.BTN_IMAGE.addEventListener("click", () => {
+            uiElements.IMAGE_UPLOAD.click();
+        });
+        uiElements.IMAGE_UPLOAD.addEventListener("change", (event) => {
+            let reader = new FileReader(),
+                file = uiElements.IMAGE_UPLOAD.files[0];
+            if(file){
+                reader.readAsDataURL(file);
+            }
+            reader.addEventListener("load", () => {
+                fabric.Image.fromURL(reader.result, (image) => {
+                    image.scale(0.5).set('flipX', true);
+                    this.canvas.add(image);
+                    this.addContextmenuListener(image);
+                });
+            });
+        });
+    }
+
+    deleteObject(){
+        this.canvas.remove(this.selectedObject);
+    }
+}
+
+export default CanvasHandler;
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,72 @@
 /* eslint-env browser */
 
+import {fabric} from 'fabric';
+import CanvasHandler from './CanvasHandler.js';
+
 function init() {
   console.log("It works");
+  let canvas = new fabric.Canvas('brainstorming-canvas', { 
+    width: 3000 , 
+    height: 3000,
+    fireRightClick: true,
+    stopContextMenu: true }),
+    canvasHandler = new CanvasHandler(canvas);
+  canvasHandler.addEventListener("rectCreated", (e) =>{
+    canvas.add(e.data);
+  });
+  canvasHandler.addEventListener("textCreated", (e) =>{
+    canvas.add(e.data);
+  });
+  canvas.on('mouse:wheel', function(opt) {
+    var delta = opt.e.deltaY;
+    var zoom = canvas.getZoom();
+    zoom *= 0.95 ** delta;
+    if (zoom > 20) zoom = 20;
+    if (zoom < 0.01) zoom = 0.01;
+    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+    opt.e.preventDefault();
+    opt.e.stopPropagation();
+  });
+  
+
+canvas.on('mouse:down', function(opt) {
+  var evt = opt.e;
+  if (evt.altKey === true){
+    this.isDragging = true;
+    this.selection = false;
+    this.lastPosX = evt.clientX;
+    this.lastPosY = evt.clientY;
+  }
+});
+canvas.on('mouse:move', function(opt) {
+  if (this.isDragging) {
+    var e = opt.e;
+    var vpt = this.viewportTransform;
+    vpt[4] += e.clientX - this.lastPosX;
+    vpt[5] += e.clientY - this.lastPosY;
+    this.requestRenderAll();
+    this.lastPosX = e.clientX;
+    this.lastPosY = e.clientY;
+  }
+});
+canvas.on('mouse:up', function(opt) {
+  // on mouse up we want to recalculate new interaction
+  // for all objects, so we call setViewportTransform
+  this.setViewportTransform(this.viewportTransform);
+  this.isDragging = false;
+  this.selection = true;
+});
+
+/*var menu = document.getElementById("contextmenu");
+
+window.addEventListener("contextmenu" , e => {
+  e.preventDefault();
+  console.log(e.target);
+  menu.style.left = `${e.pageX}px`;
+  menu.style.top = `${e.pageY}px`;
+  menu.style.display = "block";
+})
+    */
 }
 
 init();

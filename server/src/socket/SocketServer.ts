c36d29629391e4e720b420dd552fad107f2ecb29
Felix Wieland <mail@felixwie.com>
2021-03-03 12:59:06 +0100

    chore(test): Added new tests

--- server/src/socket/SocketServer.ts
+++ server/src/socket/SocketServer.ts
@@ -3,12 +3,12 @@
 
 import { Server } from "socket.io";
 
-const io = new Server({
-  path: "/ws",
-});
+const io = new Server();
 
-io.on("connection", () => {
-  console.log("Connection");
+io.on("connection", (socket) => {});
+
+io.on("message", (msg) => {
+  io.emit("message", msg);
 });
 
 export default io;

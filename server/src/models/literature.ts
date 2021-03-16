456695c03ba63f62d4c371d1b0ecb95ba67b9861
Felix Wieland <mail@felixwie.com>
2021-03-16 22:59:46 +0100

    refactor: mongoose cleanup

--- server/src/models/literature.ts
+++ server/src/models/literature.ts
@@ -1,32 +1,3 @@
-// import { Document, model, Schema } from "mongoose";
-
-// export interface ILiterature {
-//   name: string;
-//   author?: string;
-//   link?: string;
-//   pages?: number[];
-// }
-
-// export interface LiteratureDocument extends ILiterature, Document {}
-
-// export const LiteratureSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   author: {
-//     type: String,
-//   },
-//   link: {
-//     type: String,
-//   },
-//   pages: {
-//     type: [Number],
-//   },
-// });
-
-// export default model<LiteratureDocument>("Literature", LiteratureSchema);
-
 import {
   Entity,
   PrimaryGeneratedColumn,
